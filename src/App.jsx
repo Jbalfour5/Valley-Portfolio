a66cb9406ede64088d204fce8baaab872357bff7
Jbalfour5 <jbalfour5@my.bcit.ca>
2025-02-04 10:25:00 -0800

    Revert "Added popout contact to header"
    This reverts commit 6bdc807c3b4c2fad5ef5f153b0501ca432424db7.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Carousel from "./components/Carousel";
-import Contact from "./components/Contact";
 
 
 //Node Modules
@@ -13,6 +12,7 @@ import { motion } from "framer-motion";
 const App = () => {
     return (
         <motion.div
+        //Animation
         initial={{ opacity: 0, y: 20 }} 
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }} 
@@ -22,7 +22,6 @@ const App = () => {
         <About/>
         <Skills/>
         <Carousel/>
-        <Contact/>
       </motion.div>
     )
 }
